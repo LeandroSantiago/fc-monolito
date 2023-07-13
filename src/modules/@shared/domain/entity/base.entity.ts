072e828cf2195290a812ec7dd12df621de987ebc
Leandro Santiago <santiago.leandro2012@gmail.com>
2023-07-13 13:07:29 -0300

    Criando o pacote compartilhado @shared

---
+++ src/modules/@shared/domain/entity/base.entity.ts
@@ -0,0 +1,29 @@
+import Id from "../value-object/id.value-object";
+
+export default class BaseEntity {
+    private _id: Id;
+    private _createdAt: Date;
+    private _updatedAt: Date;
+    
+    constructor(id: Id){
+        this._id = id;
+        this._createdAt = new Date();
+        this._updatedAt = new Date();
+    }
+    
+    get id(): Id{
+        return this._id;
+    }
+    
+    get createdAt(){
+        return this._createdAt;
+    }
+    
+    get updatedAt(){
+        return this._updatedAt;
+    }
+    
+    set updated(value: Date){
+        this._updatedAt = value;
+    }
+}
